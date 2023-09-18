30b10630e55276850038e75c778bbb61213cadf1
jodichoo <jodichoo@users.noreply.github.com>
2023-09-18 14:32:03 +0800

    Add logic to filter html elements, save to html file

--- extractors/incomplete.js
+++ extractors/incomplete.js
@@ -2,8 +2,9 @@ import path from "path";
 import fs from "fs";
 import _ from "lodash";
 import { load } from "cheerio";
+import { parse } from "node-html-parser";
 import { axeCoreDirPath } from "../process.js";
-import { extractFileNames, reducePaths } from "../utils.js";
+import { extractFileNames, reducePaths, writeDataToFile } from "../utils.js";
 
 /*
   *********************
@@ -86,12 +87,31 @@ const incompletePostOp = (rules) => {
   const result = _.cloneDeep(rules);
   const testPath = path.join(axeCoreDirPath, "test", "integration", "rules");
 
+  // to compile all html snippets into a html file for better readability
+  const ruleInfoForHTMLFile = [];
+
+  const formHTMLSection = (heading, message, ...snippets) => {
+    const section = [`<!-- ${heading} -->`];
+    message && section.push(`<!-- ${message} -->`);
+    snippets && section.push(snippets.join("\n<!----------->\n"));
+    return section.join("\n");
+  };
+
   Object.keys(result).forEach((ruleId) => {
+    const ruleInfo = result[ruleId];
+    // only find snippets for enabled and non-experimental rules
+    const isDisabledOrExperimental =
+      (ruleInfo.hasOwnProperty("enabled") && !ruleInfo.enabled) ||
+      (ruleInfo.hasOwnProperty("experimental") && ruleInfo.experimental);
+
     const ruleTestPath = path.join(testPath, ruleId);
     if (!fs.existsSync(ruleTestPath)) {
       console.log(`%!@$!%@!%@$!@$ Nooooooooo why no test for ${ruleId}`);
+      rules[ruleId].incompleteElements = `No tests for ${ruleId}`;
+      ruleInfoForHTMLFile.push(formHTMLSection(ruleId, `No tests for ${ruleId}`, undefined));
       return;
     }
+
     const fileNames = extractFileNames(ruleTestPath);
 
     const htmlReducer = (acc, testData) => {
@@ -99,20 +119,84 @@ const incompletePostOp = (rules) => {
       return `${acc} ${testData}`;
     };
 
+    // to compile all ids that are used to test for incomplete rules
+    const jsonReducer = (status="incomplete") => {
+      return (acc, testObj) => {
+        const ids = testObj[status]; // in the form [["#incomplete1"], ["#incomplete2"], ...]
+        return ids ? [...acc, ...ids] : acc;
+      }
+    }
+
     const reducedHtml = reducePaths(
       fileNames,
       ".html",
       htmlReducer,
       ""
     ).replace(/\n/g, " ");
+    const incompleteIds = reducePaths(
+      fileNames,
+      ".json",
+      jsonReducer("incomplete"),
+      [],
+    );
     result[ruleId].wholeHtml = reducedHtml;
 
     // TODO: html not always has elements with id=incomplete
     // will need to figure some other identifier, maybe innertext or any text that has incomplete?
-    const $ = load(reducedHtml);
-    const incompleteElements = $("[id*=incomplete]");
-    result[ruleId].incompleteElements = incompleteElements.toString();
+    const dom = parse(reducedHtml);
+
+    const htmlSnippets = new Set();
+
+    if (incompleteIds.length === 0) {
+      const message = `No related tests found for incomplete rule ${ruleId}`;
+      result[ruleId].incompleteElements = message;
+      ruleInfoForHTMLFile.push(formHTMLSection(ruleId, message, undefined));
+      return;
+    }
+
+    for (let [id] of incompleteIds) {
+      const idName = id.substring(1); // remove # symbol
+      let htmlSnippet = "";
+      
+      const incompleteElements = dom.querySelectorAll(
+        `[id="${idName}"], [aria-labelledby*="${idName}"], [for="${idName}"]`
+      );
+      if (incompleteElements.length > 1) {
+        // there are other related elements e.g. <label>
+        incompleteElements.forEach((element) => {
+          htmlSnippet += element.outerHTML;
+          htmlSnippet += "\n";
+        })
+      } else {
+        const htmlElement = incompleteElements[0];
+        htmlSnippet = htmlElement.outerHTML;
+
+        // give the enclosing parent element if available (for additional context)
+        const parent = htmlElement.closest(`:not(${id})`);
+        if (parent && parent.tagName !== "BODY") {
+          htmlSnippet = parent.outerHTML;
+        }
+      }
+      htmlSnippets.add(htmlSnippet);
+    }
+
+    const htmlSnippetsArray = Array.from(htmlSnippets);
+    ruleInfoForHTMLFile.push(
+      formHTMLSection(
+        ruleId,
+        isDisabledOrExperimental
+          ? "This rule is disabled/experimental"
+          : undefined,
+        ...htmlSnippetsArray
+      )
+    );
+
+    result[ruleId].incompleteElements = htmlSnippetsArray; // add snippets to result json file
   });
+
+  const compiledHTML = ruleInfoForHTMLFile.join("\n\n\n");
+
+  writeDataToFile("results", "html_snippets_incomplete", ".html", compiledHTML);
 
   return result;
 };
